81cf9b5d487b169b62c743429f4d34abea01defa
Max <max.chen@epitech.eu>
2024-04-26 12:10:13 +0200

    Adding images and videos

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,9 +1,11 @@
 function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-2xl p-2">
+    <div className="flex flex-col justify-center items-center h-screen text-2xl gap-2 sm:gap-4 md:gap-6 p-2">
       <p>👋Hello, I'm Max</p>
-      <p className="flex text-gray">inspiring full-stack developper</p>
+      <p className="flex text-gray">full-stack developper</p>
       <p className="text-black">and this is my portfolio</p>
+
+      <img src="/imgs/Photo.png" alt="Max" className="w-44" />
     </div>
   );
 }
